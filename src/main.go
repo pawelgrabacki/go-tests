5e3c8b8449baa71ff39d13f32af92c23d437bd09
Paweł Grabacki <paw.grabacki@gmail.com>
2026-07-19 14:39:17 +0200

    further tests

--- src/main.go
+++ src/main.go
@@ -2,12 +2,18 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello World!")
+  fmt.Println("Hello World!") //comment :)
+  const PI = 3.14
 
-
-  var intNum int = 3222
+ var floatNum32 float32 = 10.1
+ var intNum int32 = 2
+ var intNuma int = 3222
   intNum = intNum + 1
   fmt.Println(intNum)
+
+  var myString string = "Hello" + " " + "World"
+  fmt.Println(myString)
+  fmt.Println(intNuma)
 }
 
 
